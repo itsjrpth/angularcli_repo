ebeca370fcf749b5d4d8a2e9dfe02517d5b131bd
gameweng <jchanchai01@gmail.com>
2019-08-06 22:41:05 +0700

    angular cli init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'cm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'contact-manager';
+
+  ngOnInit() {
+    particlesJS.load('particles', 'assets/particles.json', () => {
+      console.log('particles loaded...');
+    });
+  }
 }
